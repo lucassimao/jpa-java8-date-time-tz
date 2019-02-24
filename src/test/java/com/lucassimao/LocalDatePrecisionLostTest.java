fbe62f39d0bb8c9900a3df0284f392ecd80caa57
Lucas Simao <Lucas Simao>
2019-02-24 17:49:12 -0300

    added new test showing how com.mysql.cj.result.SqlDateValueFactory works under the hood

--- src/test/java/com/lucassimao/LocalDatePrecisionLostTest.java
+++ src/test/java/com/lucassimao/LocalDatePrecisionLostTest.java
@@ -3,12 +3,16 @@ package com.lucassimao;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.util.Calendar;
 import java.util.List;
+import java.util.Locale;
 import java.math.BigInteger;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Time;
 import java.time.LocalDate;
 import java.time.Month;
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
 import java.util.TimeZone;
 
 import javax.persistence.EntityManager;
@@ -16,6 +20,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.Query;
 
+import org.hibernate.Session;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -64,7 +69,45 @@ public class LocalDatePrecisionLostTest{
             assertEquals(16, em.find(BookEntry.class, id.longValue()).getDate().getDayOfMonth() );
 
             em.getTransaction().commit();
+            em.clear();
 
+            Session hSession =  em.unwrap(Session.class);
+            hSession.clear();
+
+            hSession.doWork( (connection)->{
+                PreparedStatement ps = connection.prepareStatement("select date from BookEntry where id=?");
+                ps.setLong(1, id.longValue());
+                ResultSet rs =  ps.executeQuery();
+                assertTrue(rs.next());
+                assertTrue( rs.getDate(1) instanceof java.sql.Date);
+                java.sql.Date dt = rs.getDate(1);
+
+                assertEquals(16, dt.getDate());
+                
+                rs.close();
+                ps.close();
+            });
+            hSession.close();
+
+    }
+
+    /**
+     * that is exactly what com.mysql.cj.result.SqlDateValueFactory does when converting a SQL DATE type to java.sql.Date
+     */
+    @Test
+    public void shouldReturnBuggyDate() {
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Fortaleza"));
+
+        Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("UTC"), Locale.US);
+        cal.set(Calendar.MILLISECOND, 0);
+        cal.setLenient(false);
+
+        cal.clear();
+        cal.set(1990, Calendar.JUNE, 17);
+        long ms = cal.getTimeInMillis();
+        Date dt = new Date(ms);
+
+        assertEquals(16, dt.getDate());
     }
 
 
